b5e16681f28917d4ae8b808b90501eed831038ed
Praful Bhojane <prafulp1996@gmail.com>
2020-12-15 16:31:10 +0530

    Add env.port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { default: Axios } = require("axios");
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 require('dotenv').config()
 
 const Twitter = require("./api/helpers/twitter.js");
